24f86e8e0086411f74a2d6645e58321e7323e216
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-07-10 17:54:08 +0200

    Created List entity.

--- src/todo/entites/List.java
+++ src/todo/entites/List.java
@@ -1,5 +1,40 @@
 package todo.entites;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "list")
 public class List {
 
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	public List() { }
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
