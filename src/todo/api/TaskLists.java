5bcbee90ef41fd12ca024b1996d23b8a1e6ce3e0
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-07-19 23:45:43 +0200

    API returns first JSON.

--- src/todo/api/TaskLists.java
+++ src/todo/api/TaskLists.java
@@ -1,17 +1,35 @@
 package todo.api;
 
+import java.util.List;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
-import todo.dao.DaoTask;
+import todo.dao.DaoTaskList;
+import todo.entites.TaskList;
 
-@Path("test")
-public class R1 {
+@Path("tasklists")
+public class TaskLists {
 
+	DaoTaskList taskListAccess;
+	
 	@GET
-	public String hello() {
-		DaoTask dao = new DaoTask();
-		return dao.getById(1).getName();
+	public Response getAllTaskLists() {
+		taskListAccess = new DaoTaskList();
+		List<TaskList> allTaskLists = taskListAccess.getAll();
+		JsonArrayBuilder jsonArrayBuilder = Json.createArrayBuilder();
+		
+		for(TaskList tl : allTaskLists)
+			jsonArrayBuilder.add(Json.createObjectBuilder()
+				.add("id", tl.getId())
+				.add("name", tl.getName())
+			);
+		
+		return Response.ok(jsonArrayBuilder.build()).build();
 	}
 	
 }
