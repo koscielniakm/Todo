658ab34728fca20d077fc1dc0636c3503dd5cf1a
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-07-22 22:55:56 +0200

    Created get by id URL.

--- src/todo/api/TaskLists.java
+++ src/todo/api/TaskLists.java
@@ -2,11 +2,11 @@ package todo.api;
 
 import java.util.List;
 
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import todo.dao.DaoTaskList;
@@ -15,21 +15,38 @@ import todo.entites.TaskList;
 @Path("tasklists")
 public class TaskLists {
 
-	DaoTaskList taskListAccess;
+	private DaoTaskList taskListAccess;
+	
+	private TaskListResponseCreator responseCreator;
 	
 	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAllTaskLists() {
-		taskListAccess = new DaoTaskList();
+		taskListAccess = getDaoTaskList();
 		List<TaskList> allTaskLists = taskListAccess.getAll();
-		JsonArrayBuilder jsonArrayBuilder = Json.createArrayBuilder();
-		
-		for(TaskList tl : allTaskLists)
-			jsonArrayBuilder.add(Json.createObjectBuilder()
-				.add("id", tl.getId())
-				.add("name", tl.getName())
-			);
-		
-		return Response.ok(jsonArrayBuilder.build()).build();
+		return getResponseCreator().createJsonFromList(allTaskLists);
+	}
+	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getTasklistById(@PathParam("id") int taskListId) {
+		taskListAccess = getDaoTaskList();
+		TaskList taskList = taskListAccess.getById(taskListId);
+		return getResponseCreator().createJsonFromSingleObject(taskList);
+	}
+	
+	private DaoTaskList getDaoTaskList() {
+		if (taskListAccess == null)
+			taskListAccess = new DaoTaskList();
+		return taskListAccess;
+	}
+	
+	private TaskListResponseCreator getResponseCreator() {
+		if (responseCreator == null)
+			responseCreator = new TaskListResponseCreator();
+		return responseCreator;
 	}
 	
 }
