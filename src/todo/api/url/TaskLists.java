c28b96cafc6b0b8a1de7639e6ec524ee9f8bcca2
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-07-23 21:37:46 +0200

    TaskList post request creating and validating.

--- src/todo/api/url/TaskLists.java
+++ src/todo/api/url/TaskLists.java
@@ -2,7 +2,10 @@ package todo.api.url;
 
 import java.util.List;
 
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -10,6 +13,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import todo.api.support.TaskListResponseCreator;
+import todo.api.support.TaskListValidator;
 import todo.dao.DaoTaskList;
 import todo.entites.TaskList;
 
@@ -36,6 +40,21 @@ public class TaskLists {
 		taskListAccess = getDaoTaskList();
 		TaskList taskList = taskListAccess.getById(taskListId);
 		return getResponseCreator().createJsonFromSingleObject(taskList);
+	}
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createTasklist(JsonObject json) {
+		String taskListName = json.getString("name");
+		if(TaskListValidator.validateName(taskListName)) {
+			TaskList createdTaskList = new TaskList();
+			createdTaskList.setName(taskListName);
+			getDaoTaskList().create(createdTaskList);
+			return Response.ok().build();
+		} else {
+			return Response.status(400).build();
+		}
 	}
 	
 	private DaoTaskList getDaoTaskList() {
