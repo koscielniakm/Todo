2bc46f72d53089ae1b62ee380d39addf3e5ff1dc
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-07-25 23:23:03 +0200

    Working PUT / DELETE requests for Task Api. Completed Api.

--- src/todo/api/url/TaskApi.java
+++ src/todo/api/url/TaskApi.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import javax.json.JsonObject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -62,16 +64,38 @@ public class TaskApi implements CrudApi {
 			return Response.status(400).build();
 	}
 
+	@PUT
+	@Path("/")
 	@Override
+	@Consumes(MediaType.APPLICATION_JSON)
 	public Response update(JsonObject json) {
-		// TODO Auto-generated method stub
-		return null;
+		Task taskToUpdate = new Task();
+		Integer id = json.getInt("id");
+		String name = json.getString("name");
+		String description = json.getString("description");
+		String status = json.getString("status");
+		taskToUpdate.setId(id);
+		taskToUpdate.setName(name);
+		taskToUpdate.setDescription(description);
+		taskToUpdate.setStatus(status);
+		if (getService().update(taskToUpdate)) {
+			return Response.ok().build();
+		} else {
+			return Response.status(400).build();
+		}
 	}
 
+	@DELETE
+	@Path("/")
 	@Override
+	@Consumes(MediaType.APPLICATION_JSON)
 	public Response remove(JsonObject json) {
-		// TODO Auto-generated method stub
-		return null;
+		int taskToDeleteId = json.getInt("id");
+		if (getService().remove(taskToDeleteId)) {
+			return Response.ok().build();
+		} else {
+			return Response.status(400).build();
+		}
 	}
 	
 	private TaskResponseCreator getResponseCreator() {
