ff653ff9b1087f38aa7845de7d6d07578104fb9e
Mateusz Kościelniak <koscielniakmateusz@onet.pl>
2018-07-27 21:29:08 +0200

    Application load to HTML menu tasklists from database.

--- WebContent/js/functions.js
+++ WebContent/js/functions.js
@@ -1,12 +1,23 @@
-$('#btn').click(function(){
+function getTasklistRowDiv(id, taskListName) {
+  return '<div id="' + id + '">p><span class="fontello icon-list"></span>' + taskListName +'</p></div>';
+}
+
+function loadTaskListMenu() {
   $.ajax({
     url: "http://localhost:8080/TodoWeb/todo/tasklist/",
     success: function(data) {
       JSON.stringify(data);
-      $("#result").append(JSON.stringify(data));
+      for(let i = 0; i < data.length; i++) { 
+        $('#tasklists').append(
+        '<div class="sidebar-row-hover row" id="' + data[i]['id'] +
+        '"><p><span class="fontello icon-list"></span>' + data[i]['name'] +
+        '</p></div>');
+      }
   },
     error: function() {
-      alert("Error in the req+uest.");
+      alert("Error in the request.");
     }
   });
-});
+};
+
+loadTaskListMenu();
